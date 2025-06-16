a421ae1dfa99c872ea6cd8c567c4a36a5ac1b8ce
Muhammad Sultan Ihsan <71641066+opick-wq@users.noreply.github.com>
2025-06-16 18:21:29 +0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,13 +1,21 @@
 # test_app.py
 from app import app
 
+
 def test_home():
+    """
+    Menguji endpoint utama (/).
+    """
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
     assert b'Selamat datang di Flask REST API!' in response.data
 
+
 def test_status():
+    """
+    Menguji endpoint status (/status).
+    """
     client = app.test_client()
     response = client.get('/status')
     assert response.status_code == 200
