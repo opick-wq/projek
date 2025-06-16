5a1503595bd275afd152ba7ae3b630b8af2fbafc
opick-wq <ikgsanikhsan93@gmail.com>
2025-06-16 19:09:14 +0700

    tambah

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 import time
 from prometheus_client import generate_latest, Counter, Histogram, Gauge
 import random
